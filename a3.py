94a88bd5c6ef759c6ba8a372fd2b164414be478b
Patrick Nyman <pnyman@cps.edu>
2025-10-07 11:19:25 -0500

    Imported dictionary.json

--- a3.py
+++ a3.py
@@ -1,1 +1,19 @@
+# import dictionary database JSON file
+import json
 
+with open("dictionary.json", "r", encoding="utf-8") as file:
+    dictionary_data = json.load(file)
+
+# import other files
+from match import match
+from typing import List, Tuple, Callable, Any
+
+# main code
+def define(matches: List[str]) -> List[str]:
+    word = 
+
+# asserts
+if __name__ == "__main__":
+    assert define(["woolsey"]) == ["Linsey-woolsey."], "failed define test"
+
+    print("all tests passed!")
