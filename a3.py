a49c2ac3d23fd00820e1c0d5bb67b90fa02e039c
Alex <azhou5@cps.edu>
2025-10-14 13:22:57 -0500

    Update a3.py

--- a3.py
+++ a3.py
@@ -89,8 +89,29 @@ if __name__ == "__main__":
     print("all tests passed!")
 while True:
     word = input("Enter word(/help for other commands): ")
-    if word == "/help":
-        print("!random - Gives you a random word\n!hangman - Play hangman with the chatbot\n!Guess - Guess the word by its meaning")
-    elif word == "!random":
-         print(get_random_word())
+    if "!" in word:
+        if word.lower() == "!random":
+            temp = get_random_word()
+            print(temp + "\n" + get_random_word(temp))
+        elif word.lower() == "!guess":
+            guessActive = True
+            rWord = get_random_word() #Edit the function to only return the definition if the user enters this command
+            while guessActive == True:
+                print(word_by_meaning(rWord))
+                user = input("Guess the word: ")
+                    if user.lower() == rWord:
+                        print("Correct")
+                        guessActive = False
+                    else:
+                        print("Incorrect")
+    else:
+        print(meaning_by_word(word))
+
+#Dont delete:
+  #  if word == "/help":
+   #     print("!random - Gives you a random word\n!hangman - Play hangman with the chatbot\n!Guess - Guess the word by its meaning")
+    #elif word == "!random":
+     #    print(get_random_word())
+
+#Can someone finish the other two functions
 
